933999e9f3cceb20c106a63e7ca35d4878d97328
Khoon Hong Lee <leekhoonhong15122001@gmail.com>
2021-12-10 16:47:05 +0800

    sien

---
+++ Calculator/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using WpfApplication1.Annotations;
+
+namespace WpfApplication1.Core
+{
+    class ObservableObject: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
